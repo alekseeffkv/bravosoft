be000b81d1afb37d7414fdc1c696d4dd6f8e35f3
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-10-05 11:49:34 +0300

    Add Material UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,12 @@
-const App = () => {
-  return <div></div>;
+import { FC } from 'react';
+import { CssBaseline } from '@mui/material';
+
+const App: FC = () => {
+  return (
+    <>
+      <CssBaseline />
+    </>
+  );
 };
 
 export default App;
